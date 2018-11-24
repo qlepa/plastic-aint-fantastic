167d654de70360cda5fb8b4158fe38d1467d0950
lukjaki <l.jakimow@gmail.com>
2018-11-24 21:34:20 +0100

    added features

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './scss/main.scss';
+import SummaryCalc from "./components/summary"
 
 class App extends Component {
    constructor(props) {
    super(props);
     this.state= {
-        status: "Start"
+        status: "Start",
+        cups: 1,
+        fork: 0,
+        knives: 0,
+        plates: 0,
+        straws: 150,
+        bottles: 30,
+        kilos: 150,
+        party: 'bufet',
+        ppl: 3,
+        hours: 3
+
     }
    }
    render(){
-       switch ( this.state.status) {
+       switch (this.state.status) {
            case "Start":
                return (
-                   <div>
-                       Hello
-                   </div>
+                   <SummaryCalc data={this.state}/>
                )
        }
 
