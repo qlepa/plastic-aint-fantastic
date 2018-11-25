01910318a42934ec1536ab6db4bc012f5f894a7b
Tomasz Klepacki <t.klepacki@gmail.com>
2018-11-25 11:45:38 +0100

    Updates

---
+++ precache-manifest.327ab24c464225a26d8dabf5967267ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebf5d87cbb79be6b40d6",
+    "url": "/static/css/main.bdd7a336.chunk.css"
+  },
+  {
+    "revision": "ebf5d87cbb79be6b40d6",
+    "url": "/static/js/main.ebf5d87c.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b9e7c1ae792e37a7b1c6d0231bd9ee47",
+    "url": "/static/media/background2.b9e7c1ae.png"
+  },
+  {
+    "revision": "92e285651c0165b1a43225c1ec4765c0",
+    "url": "/index.html"
+  }
+];
